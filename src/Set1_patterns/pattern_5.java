e38954cc227b970fd4da25a5ea32d5476d3ac6f7
Mathiveendhan <Mathiveendhan003@gmail.com>
2025-04-30 11:48:47 +0530

    Pattern

--- src/Set1_patterns/pattern_5.java
+++ src/Set1_patterns/pattern_5.java
@@ -7,7 +7,7 @@
 package Set1_patterns;
 public class pattern_5 {
     public static void main(String[] args) {
-        int n=5;
+        int n=5 ;
         for(int i=1;i<=n;i++){
             int num=i;
             for(int j=1;j<=n;j++){
